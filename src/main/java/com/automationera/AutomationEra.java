16470df6ac927bb3c386a3bd4024dab0ddf30cdd
Albertchen857 <albertchen857@outlook.com>
2025-06-01 17:29:30 +1000

    Revert "s"
    This reverts commit c98c3b9f420549fac8ac4e66be0ed0fea3a07b0c.

--- src/main/java/com/automationera/AutomationEra.java
+++ src/main/java/com/automationera/AutomationEra.java
@@ -1,33 +1,24 @@
 package com.automationera;
 
-import com.automationera.advance.AdvancementTriggerHandler;
-import com.automationera.advance.FullStackCriterion;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents;
-import net.minecraft.advancement.criterion.Criteria;
-import net.minecraft.server.network.ServerPlayerEntity;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AutomationEra implements ModInitializer {
 	public static final String MOD_ID = "automationera";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-	public static final FullStackCriterion FULL_STACK_CRITERION = new FullStackCriterion();
 
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-		Criteria.register(FULL_STACK_CRITERION);
-		// 添加每刻检查
-		ServerTickEvents.START_SERVER_TICK.register(server -> {
-			if (server.getTicks() % 100 == 0) {
-				for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
-					AdvancementTriggerHandler.checkFullStacks(player);
-				}
-			}
-		});
+
+		LOGGER.info("Hello Fabric world!");
 	}
 }
--- src/main/java/com/automationera/AutomationEraDataGenerator.java
+++ src/main/java/com/automationera/AutomationEraDataGenerator.java
@@ -2,12 +2,10 @@ package com.automationera;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-import com.automationera.advance.AdvancementsProvider;
 
 public class AutomationEraDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
-		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
-		pack.addProvider(AdvancementsProvider::new);
+
 	}
 }
